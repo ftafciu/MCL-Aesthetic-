61d5dcf9ac076359fab8e61b0d25379d58f31d6d
Aldrin <acifliku21@epoka.edu.al>
2024-12-01 07:51:01 +0100

    Added backend for resetting password

--- server/routers/authorization.js
+++ server/routers/authorization.js
@@ -4,6 +4,7 @@ const app = express();
 const login_controller = require("../controllers/userProxy");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -13,5 +14,56 @@ app.get("/authorize", (req, res) => {
     login_controller.serveMyInfo(req, res);
   });
 });
+
+app.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+  userModel.findOne({ email: email }).then(async (user) => {
+    if (!user) {
+      return res.send({ status: "User does not exist!" });
+    }
+    const token = await login_controller.token_issue(user);
+
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.SERVICE_MAIL,
+        pass: process.env.SERVICE_MAIL_PASSWORD,
+      },
+    });
+
+    var mailOptions = {
+      from: process.env.SERVICE_MAIL,
+      to: email,
+      subject: "Reset your password",
+      text: `${process.env.REACT_APP_BACKEND_URL}/auth/new-password/${user._id}/${token.accessToken}`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        return res.send({ status: "Success" });
+      }
+    });
+  });
+});
+
+app.post("/reset-password/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
+
+  const check = await tokenManager.tokenChecker(token);
+  if (!check.result) {
+    return res.json({ status: "Error with token" });
+  } else {
+    bcrypt
+      .hash(password, 10)
+      .then((hash) => {
+        login_controller.editUser(id, { password: hash });
+      })
+      .catch((err) => res.send({ status: err }));
+  }
+});
+
 
 module.exports = app;
